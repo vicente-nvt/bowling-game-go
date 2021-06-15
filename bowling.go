cc7f7592da32cb1fd28682f1f15aa2728d2266ba
Caio Silva <rasecoiac03@gmail.com>
2021-06-15 17:19:51 -0300

    calculate frame each two throws

--- bowling.go
+++ bowling.go
@@ -2,9 +2,19 @@ package main
 
 type Scorer struct {
 	Throws []int
+	Frames []int
 }
 
 func (s *Scorer) AddThrow(numberOfPinsDown int) {
-
 	s.Throws = append(s.Throws, numberOfPinsDown)
+
+	throwsLen := len(s.Throws)
+	if throwsLen > 0 && throwsLen%2 == 0 {
+		frameThrows := s.Throws[throwsLen-1] + s.Throws[throwsLen-2]
+		s.Frames = append(s.Frames, frameThrows)
+	}
+}
+
+func (s *Scorer) GetFrameScore(index int) (int, error) {
+	return 0, nil
 }
--- bowling_test.go
+++ bowling_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestShouldAddAThrowIntoScorer(t *testing.T) {
 	numberOfPinsDown := 1
+
 	scorer := &Scorer{}
 
 	scorer.AddThrow(numberOfPinsDown)
@@ -11,4 +12,22 @@ func TestShouldAddAThrowIntoScorer(t *testing.T) {
 	if scorer.Throws[0] != numberOfPinsDown {
 		t.Errorf("The number of pins down for the first throw should be %v, but actually is %v", numberOfPinsDown, scorer.Throws[0])
 	}
+}
+
+func TestShouldCalculateThePointsInAFrame(t *testing.T) {
+	numberOfPinsDownInFirstThrow := 1
+	numberOfPinsDownInSecondThrow := 4
+	expectedScoreInFirstFrame := numberOfPinsDownInFirstThrow + numberOfPinsDownInSecondThrow
+	scorer := &Scorer{}
+
+	scorer.AddThrow(numberOfPinsDownInFirstThrow)
+	scorer.AddThrow(numberOfPinsDownInSecondThrow)
+
+	if scorer.Frames[0] != expectedScoreInFirstFrame {
+		t.Errorf("The score of the first frame should be %v, but actually is %v", expectedScoreInFirstFrame, scorer.Frames[0])
+	}
+}
+
+func TestShouldValidateIfFramesExists(t *testing.T) {
+
 }
